1b2b9f2cef9856bb443c3bf32595e6de06eab79e
ElectroBean <dburgan@live.com>
2017-11-29 21:29:17 +1100

    Added Game Manager Added Time Adders Added Score Added ingame UI

---
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    [Header("Timer stuff")]
+    public float StartingTime;
+    float TimerCount;
+    public Text TimerText;
+    int counter;
+
+    public Text AddTime;
+
+
+	// Use this for initialization
+	void Start () {
+        TimerCount = StartingTime;
+        AddTime.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        counter = (int)TimerCount;
+        TimerText.text = counter.ToString();
+        TimerCount -= Time.deltaTime;
+        
+	}
+    
+
+    public IEnumerator AddToTime(float a_TimeAdded)
+    {
+        AddTime.enabled = true;
+        AddTime.text = " + " + a_TimeAdded.ToString();
+        StartCoroutine(FadeTextToZeroAlpha(2, AddTime));
+        yield return new WaitForSeconds(2);
+        AddTime.enabled = false;
+        TimerCount += a_TimeAdded;
+    }
+
+    public IEnumerator FadeTextToZeroAlpha(float t, Text i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 1);
+        while (i.color.a > 0.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t));
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager/TimeAdders.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeAdders : MonoBehaviour {
+
+    GameManager gm;
+    public float TimeToAdd;
+
+    private void Awake()
+    {
+        gm = FindObjectOfType<GameManager>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            gm.StartCoroutine(gm.AddToTime(TimeToAdd));
+            Destroy(gameObject);
+        }
+    }
+}
